6421d51929665221beb79709d9b309e76fb11013
YoaN80490 <147514201+YoaN80490@users.noreply.github.com>
2023-12-13 11:54:42 +0100

    11h54 13/12/2023

---
+++ exercice js/exercice.js
@@ -0,0 +1,60 @@
+/*var texteAlert = prompt("je mange du chien:")
+
+let a = 5
+let b = 6
+var result = a + b 
+var calcul = window.alert(result)
+
+let note_maths = 11;
+let note_francais = 9;
+let note_hg = 16;
+let moyenne = (note_maths + note_francais + note_hg) / 3 ;
+alert('La moyenne est de ' + (moyenne));
+
+var budget = 2000
+var achats = 1500
+
+var economie = budget - achats
+
+if(budget - achats > 0){
+    window.alert( 'Le budget permet de payer les achats, il reste ' + (economie)+ '€')
+}
+else{
+    window.alert( 'Le budget ne permet pas de payer les achats')
+}
+
+const prix = document.getElementById('dialogue')
+var exercice = parseFloat(prompt('quel est le prix hors taxe de votre achat'))
+var resultat = ((exercice * 20)/100)+ exercice
+prix.innerHTML=('le prix est de ' + resultat + ' €')
+
+
+const multiple = document.getElementById('dialogue')
+
+
+for(var x = 10; x <= 1000;x+=10){
+    multiple.innerHTML+=x + '<br>'
+
+}
+
+
+
+var age = parseInt(prompt('Quel age as tu?'))
+
+if(age < 18){
+    window.alert('tu n\'as pas 18 ans, dégage')
+}
+else{
+    window.alert('une chaudasse a moin de 5km de chez toi')
+}
+
+
+const dialogue = document.getElementById('dialogue')
+
+var codePostal = 7000
+
+while(codePostal < 8000){
+    dialogue.innerHTML += '7' + codePostal + '<br>'
+    codePostal++
+}
+*/
